35c3f69c4ae47058dfdb16162333f0ff18a18d6d
Robert Schönherr <admin@rbs90.de>
2015-10-15 21:04:00 +0200

    finished all tasks from assignment 1

---
+++ assignment_1/3/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+	if (argc != 2){
+		printf("Usage: ./result <0-255>\n");
+		return 1;
+	}
+
+	unsigned char byte_in = (unsigned char) atoi(argv[1]);
+	printf("%d\n", (unsigned char) ~byte_in);
+	return 0;
+}
---
+++ assignment_1/4/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+	if (argc != 2){
+		printf("Usage: ./bitreverse <0-255>\n");
+		return 1;
+	}
+
+	int in_value = atoi(argv[1]);
+	int count = 0;
+
+	while(in_value > 0){
+		count += in_value & 1;
+		in_value = in_value >> 1;
+	}
+
+	printf("%d\n", count);
+	return 0;
+}
